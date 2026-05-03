a566c562053f661e9d4eaa8b975fa651733d70e5
HanaQaisara <HANA.QAISARA.KHAIRUL@student.mmu.edu.my>
2026-05-03 14:20:19 +0800

    Added decoration items

---
+++ shopdecoration_standalone.py
@@ -0,0 +1,11 @@
+import pygame
+
+# decorations (name, cost, position, colour)
+decorations = [
+    ("Lamp", 200, (100, 400), (255, 255, 0)),                     # yellow lamp
+    ("Plant", 150, (300, 400), (0, 200, 0)),                      # green plant
+    ("Scratching Post Cat", 300, (500, 200), (150, 75, 0))        # brown scratching post
+]
+
+
+
