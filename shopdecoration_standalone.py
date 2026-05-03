0b620ada11bc1b9b23382d2884cb6aef287a81c6
HanaQaisara <HANA.QAISARA.KHAIRUL@student.mmu.edu.my>
2026-05-03 14:34:56 +0800

    Create button with text inside

--- shopdecoration_standalone.py
+++ shopdecoration_standalone.py
@@ -7,5 +7,14 @@ decorations = [
     ("Scratching Post Cat", 300, (500, 200), (150, 75, 0))        # brown scratching post
 ]
 
+def draw_button(screen, font, text, x, y, w, h):              # x,y: position of the button  
+    rect = pygame.Rect(x, y, w, h)                            # w,h: width and height
+    pygame.draw.rect(screen, (200, 200, 200), rect)           # draw rectangle w light gray
+    label = font.render(text, True, (0, 0, 0))                # true for anti-aliasing # black colour
+    screen.blit(label, (x+10, y+10))                          # makes the text centered inside button
+    return rect                                               # clickable
+
+
+
 
 
