717f69f0240183ec8b7b8a027799bf7c7081927d
mikaimansengol <arifmika272@gmail.com>
2026-05-28 01:18:07 +0800

    Update asset_loader.py
    added new cup/cursor states

--- asset_loader.py
+++ asset_loader.py
@@ -3,7 +3,6 @@ from path_helper import get_path
 from settings import WIDTH, HEIGHT
 
 def initialize_audio(volume):
-    """Starts background mixer music."""
     music_playing = False
     try:
         pygame.mixer.music.load(get_path("music.mp3")) 
@@ -15,7 +14,6 @@ def initialize_audio(volume):
     return music_playing
 
 def load_visual_assets():
-    """Loads background imagery and custom cursor pointers."""
     try:
         bg_img = pygame.image.load(get_path("cafe_1_background.png")).convert()
         bg_img = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
@@ -28,4 +26,50 @@ def load_visual_assets():
     except:
         cursor_img = None
 
-    return bg_img, cursor_img
+    try:
+        kitchen_bg = pygame.image.load(get_path("kitchen_background.png")).convert()
+        kitchen_bg = pygame.transform.scale(kitchen_bg, (WIDTH, HEIGHT))
+    except:
+        kitchen_bg = None
+
+    # --- UPDATED: REAL PNG CURSOR ASSET LOADER ---
+    cursor_assets = {}
+    cursor_size = (60, 60)
+
+    # 1. Empty Cup
+    try:
+        cursor_assets["EMPTY_CUP"] = pygame.image.load(get_path("cup_empty.png")).convert_alpha()
+        cursor_assets["EMPTY_CUP"] = pygame.transform.scale(cursor_assets["EMPTY_CUP"], cursor_size)
+    except:
+        surf = pygame.Surface(cursor_size, pygame.SRCALPHA)
+        pygame.draw.rect(surf, (200, 200, 250), (10, 10, 40, 40), border_radius=5)
+        cursor_assets["EMPTY_CUP"] = surf
+
+    # 2. Chocolate Cup
+    try:
+        cursor_assets["CHOCOLATE"] = pygame.image.load(get_path("cup_chocolate.png")).convert_alpha()
+        cursor_assets["CHOCOLATE"] = pygame.transform.scale(cursor_assets["CHOCOLATE"], cursor_size)
+    except:
+        surf = pygame.Surface(cursor_size, pygame.SRCALPHA)
+        pygame.draw.rect(surf, (101, 67, 33), (10, 10, 40, 40), border_radius=5)
+        cursor_assets["CHOCOLATE"] = surf
+
+    # 3. Strawberry Cup
+    try:
+        cursor_assets["STRAWBERRY"] = pygame.image.load(get_path("cup_strawberry.png")).convert_alpha()
+        cursor_assets["STRAWBERRY"] = pygame.transform.scale(cursor_assets["STRAWBERRY"], cursor_size)
+    except:
+        surf = pygame.Surface(cursor_size, pygame.SRCALPHA)
+        pygame.draw.rect(surf, (255, 182, 193), (10, 10, 40, 40), border_radius=5)
+        cursor_assets["STRAWBERRY"] = surf
+
+    # 4. Boba Milk Tea Cup
+    try:
+        cursor_assets["BOBA MILK TEA"] = pygame.image.load(get_path("cup_boba.png")).convert_alpha()
+        cursor_assets["BOBA MILK TEA"] = pygame.transform.scale(cursor_assets["BOBA MILK TEA"], cursor_size)
+    except:
+        surf = pygame.Surface(cursor_size, pygame.SRCALPHA)
+        pygame.draw.rect(surf, (245, 222, 179), (10, 10, 40, 40), border_radius=5)
+        cursor_assets["BOBA MILK TEA"] = surf
+
+    return bg_img, cursor_img, kitchen_bg, cursor_assets
